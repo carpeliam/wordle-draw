44d16a6f706096c58bee29ab44f462fec53e94f4
Liam Morley <liam@carpeliam.com>
2022-02-09 21:55:52 -0500

    Initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Object.assign(navigator, {
+  clipboard: {
+    writeText: () => {},
+  },
 });
+
+describe('App', () => {
+  beforeEach(() => {
+    jest.spyOn(navigator.clipboard, 'writeText');
+  });
+  it('should initially show a 5x6 grid of black squares', () => {
+    render(<App />);
+
+    expect(screen.getAllByRole('row')).toHaveLength(6);
+
+    const squares = screen.getAllByRole('cell');
+    expect(squares).toHaveLength(5 * 6);
+    squares.forEach((square: HTMLElement) => {
+      expect(square.textContent).toEqual('⬛️');
+    });
+  });
+
+  it('should turn a square from black to yellow to green to black on click', () => {
+    render(<App />);
+
+    const firstCell = screen.getAllByRole('cell')[0];
+    const button = buttonInCell(firstCell);
+    
+    userEvent.click(button);
+    expect(firstCell.textContent).toEqual('🟨');
+    userEvent.click(button);
+    expect(firstCell.textContent).toEqual('🟩');
+    userEvent.click(button);
+    expect(firstCell.textContent).toEqual('⬛️');
+  });
+
+  it('should copy the grid state to the clipboard', () => {
+    render(<App />);
+
+    const cells = screen.getAllByRole('cell');
+    const yellowCellIndexes = [5, 7, 9, 10, 11, 12, 15, 17, 19, 20, 22, 24];
+    const greenCellIndexes = [25, 26, 27, 28, 29];
+    yellowCellIndexes.forEach(index => userEvent.click(buttonInCell(cells[index])));
+    greenCellIndexes.forEach(index => {
+      userEvent.click(buttonInCell(cells[index]));
+      userEvent.click(buttonInCell(cells[index]));
+    });
+
+    userEvent.click(screen.getByRole('button', { name: 'Copy!' }));
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(`⬛️⬛️⬛️⬛️⬛️
+🟨⬛️🟨⬛️🟨
+🟨🟨🟨⬛️⬛️
+🟨⬛️🟨⬛️🟨
+🟨⬛️🟨⬛️🟨
+🟩🟩🟩🟩🟩`);
+  });
+});
+
+function buttonInCell(cell: HTMLElement) {
+  const { getByRole } = within(cell);
+  return getByRole('button');
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
+import React, { ChangeEvent, MouseEventHandler, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Cell({ color, location, onUpdate }: { color: string, location: [number, number], onUpdate: Function }) {
+  // const [color, setColor] = useState('⬛️');
+  function changeColor() {
+    let newColor: string;
+    switch (color) {
+      case '⬛️':
+        newColor = '🟨';
+        break;
+      case '🟨':
+        newColor = '🟩';
+        break;
+      case '🟩':
+      default:
+        newColor = '⬛️';
+        break;
+    }
+    onUpdate(location, newColor);
+  //   setColor(newColor);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div role="cell" className="tile">
+      <button onClick={changeColor}>{color}</button>
+    </div>
+  )
+}
+
+
+function Row({ data, row, onUpdate }: { data: string[], row: number, onUpdate: Function }) {
+  return (
+    <div role="row" className="row">
+      {data.map((color, i) => <Cell key={`c${row}-${i}`} color={color} location={[row, i]} onUpdate={onUpdate} />)}
+    </div>
+  );
+}
+
+/*
+Wordle 235 1/6
+
+🟩🟩🟩🟩🟩
+*/
+
+const defaultGrid: string[][] = new Array(6).fill(new Array(5).fill('⬛️'));
+
+function gridString(grid: string[][]) {
+  return grid.map(row => row.join('')).join('\n');
+}
+
+function App() {
+  const [grid, setGrid] = useState(defaultGrid);
+  function update(location: [number, number], newColor: string) {
+    const [x, y] = location;
+    setGrid(grid.map((row, i) => (
+      row.map((cell, j) => (i === x && j === y) ? newColor : cell)
+    )));
+  }
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(gridString(grid));
+  }
+  return (
+    <div id="game">
+      <div role="table" className="board">
+        {grid.map((cells, i) => {
+          return <Row key={`r${i}`} data={cells} row={i} onUpdate={update} />
+        })}
+      </div>
+      <button onClick={copyToClipboard}>Copy!</button>
     </div>
   );
 }
